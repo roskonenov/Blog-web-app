2413cbad8c208c07a90c4c8e6e50b08c45e155b7
roskonenov <roskonenov@gmail.com>
2026-04-30 20:24:10 +0300

    add dinamically rendered data in hero section

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import blogData from "./data/blogData.js";
+import truncateText from "./data/utils/truncateText.js";
 
 const app = express();
 const port = 3000;
@@ -6,7 +8,7 @@ const port = 3000;
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {blogData: blogData, truncateText: truncateText});
 })
 
 app.listen(port, () => {
